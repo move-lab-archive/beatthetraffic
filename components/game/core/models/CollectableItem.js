5f56291fb98f703870e9f29bb1de4119a07fba0a
Thibault Durand <tibo.durand@gmail.com>
2017-11-27 20:11:40 +0200

    WIP: make banana blink

--- components/game/core/models/CollectableItem.js
+++ components/game/core/models/CollectableItem.js
@@ -1,4 +1,4 @@
-import { TweenLite, TweenMax } from 'gsap'
+import { TimelineMax, Linear } from 'gsap'
 import GameEngineStateManager from '../../../../statemanagement/app/GameEngineStateManager'
 import CollectableItemsEngine from '../engines/CollectableItemsEngine'
 
@@ -36,11 +36,42 @@ class CollectableItem {
   }
 
   animate () {
-    this.anim = TweenMax.to(this, 1, {
-      currentFrame: CollectableItemsEngine.getNbFrames(this.type),
-      ease: SteppedEase.config(CollectableItemsEngine.getNbFrames(this.type)),
-      repeat: -1
-    })
+    this.anim = new TimelineMax({ onComplete: () => this.destroy() })
+
+    // Todo make that configurable
+    const durationBeforeDisapearing = 10
+    const blinkingDuration = 0.5
+    const timeStartBlinking = durationBeforeDisapearing / 2
+    const repeatBlinking =
+      (durationBeforeDisapearing - timeStartBlinking) / blinkingDuration
+
+    this.anim
+      // first part of timeline is the rotation of the collectable item
+      .to(
+        this,
+        1,
+        {
+          currentFrame: CollectableItemsEngine.getNbFrames(this.type),
+          ease: SteppedEase.config(
+            CollectableItemsEngine.getNbFrames(this.type)
+          ),
+          repeat: durationBeforeDisapearing - 0.5 //  little hack because we don't want this anim to finish after the blinking one
+        },
+        0
+      )
+      // second part is the blinking that happen
+      // TODO would be great to have the blinking accelerate
+      .to(
+        this,
+        blinkingDuration / 2,
+        {
+          opacity: 0,
+          yoyo: true,
+          repeat: repeatBlinking,
+          ease: Power0.easeNone
+        },
+        timeStartBlinking
+      )
     this.anim.play()
   }
 
