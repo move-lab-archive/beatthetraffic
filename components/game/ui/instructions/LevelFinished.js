06c737cf88063244a0bcc203984b9e1a4076ebc0
Thibault Durand <tibo.durand@gmail.com>
2017-12-08 15:52:56 +0100

    WIP: remove useless code

--- components/game/ui/instructions/LevelFinished.js
+++ components/game/ui/instructions/LevelFinished.js
@@ -14,48 +14,12 @@ class LevelFinished extends Component {
   }
 
   render () {
-    return (
-      <div className='instructions-level-finished'>
-        {/* <div className='title'>CONGRAT!</div>
-        <div className='message'>
-          You finished level {this.props.currentLevel} with {this.props.score}{' '}
-          ⭐️ !
-        </div>
-        <Button
-          title={`Next level`}
-          large
-          transparent
-          onClick={() =>
-            this.props.dispatch(loadLevel(this.props.currentLevel + 1))
-          }
-        />
-        <style jsx>{`
-          .instructions-level-finished {
-            display: flex;
-            flex: 1;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            color: white;
-            padding: 1rem;
-          }
-          .title {
-            font-size: 6rem;
-          }
-
-          .message {
-            text-align: center;
-            margin-bottom: 1.5rem;
-          }
-        `}</style> */}
-      </div>
-    )
+    return <div className='instructions-level-finished' />
   }
 }
 
 export default connect(state => {
   return {
-    score: state.game.get('score'),
     currentLevel: state.game.get('currentLevel')
   }
 })(LevelFinished)
