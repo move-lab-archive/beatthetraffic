0897b86c5f5a2688fad49faa6dd447fbe5010c16
Thibault Durand <tibo.durand@gmail.com>
2018-03-14 11:38:48 -0300

    Evo: refactor landing page responsive code

--- components/game/ui/Landing.js
+++ components/game/ui/Landing.js
@@ -68,34 +68,28 @@ class Landing extends Component {
   render () {
     return (
       <div className='game-landing'>
-        <h1 className='landing-headline'>
-          BEAT THE TRAFFIC
-          <span className='city-var city'>{this.props.selectedCity}</span>
-        </h1>
-        <Unicorn />
-        <BtnLanding
-          loaded={this.props.isGameReadyToPlay}
-          onClick={this.handleStartGame}
-        />
-        <img className='tree' src='/static/assets/landing/asset-tree.png' />
+        <div className='landing-content'>
+          <img className='tree' src='/static/assets/landing/asset-tree.png' />
+          <h1 className='landing-headline'>
+            BEAT THE TRAFFIC
+            <span className='city-var city'>{this.props.selectedCity}</span>
+          </h1>
+          <BtnLanding
+            loaded={this.props.isGameReadyToPlay}
+            onClick={this.handleStartGame}
+          />
+        </div>
         {this.state.javascriptLoaded && (
           <div>
             <ChangeCityButtonLanding />
+            <LeftCloud />
+            <RightCloud />
             <div className='about'>
               <h4>
                 <a href='https://www.google.de/' target='_blank'>
                   OPEN DATA CAM
                 </a>
               </h4>
-            </div>
-
-            <LeftCloud />
-            <RightCloud />
-            <div className='mobility-assets'>
-              <img
-                className='unicorn'
-                src='/static/assets/landing/asset-unicorn.png'
-              />
             </div>
           </div>
         )}
@@ -118,11 +112,16 @@ class Landing extends Component {
             align-items: center;
           }
 
-          .landing-headline {
-            position: absolute;
-            margin-top: -20px;
-            width: 80%;
+          .landing-content {
             z-index: 2;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: -50px;
+          }
+
+          .landing-headline {
+            width: 80%;
             animation: fadeInHeadline 1.3s;
             color: #262626;
             text-align: center;
@@ -131,6 +130,13 @@ class Landing extends Component {
           .city-var {
             display: block;
             color: #ff3bff;
+          }
+
+          .tree {
+            animation: fadeInHeadline 1.3s;
+            width: 7rem;
+            opacity: 1;
+            left: 50%;
           }
 
           .about {
@@ -143,39 +149,6 @@ class Landing extends Component {
           }
           .about:hover {
             color: #ff3bff;
-          }
-
-          .powered {
-            position: fixed;
-            top: 1.5rem;
-            animation: fadeIn 2s;
-            z-index: 14;
-            margin: 0;
-            padding: 0;
-            left: 50%;
-            transform: translateX(-50%);
-            text-align: center;
-          }
-
-          .mobility-assets .unicorn {
-            animation: fadeIn 2s;
-            width: 8.5rem;
-            opacity: 1;
-            top: 0.7rem;
-            left: 0.5rem;
-            z-index: 0;
-            position: fixed;
-            display: none;
-          }
-
-          .tree {
-            animation: fadeInHeadline 1.3s;
-            width: 7rem;
-            opacity: 1;
-            left: 50%;
-            transform: translateX(-50%) translateY(-160px);
-            z-index: 100;
-            position: absolute;
           }
 
           @keyframes fadeIn {
@@ -226,18 +199,29 @@ class Landing extends Component {
             }
           }
 
-          @media (min-width: 600px) {
-            .landing-headline {
-              width: 525px;
-              text-align: center;
-            }
-            .tree {
+          @media (max-height: 560px) {
+            .landing-content {
+              margin-top: -100px;
             }
           }
 
-          @media (max-height: 400px) {
+          @media (max-height: 500px) {
             .tree {
-              transform: translateX(-50%) translateY(-90px);
+              display: none;
+            }
+
+            .landing-content {
+              margin-top: -110px;
+            }
+          }
+
+          @media (max-height: 460px) {
+            .landing-content {
+              margin-top: -140px;
+            }
+
+            .landing-headline {
+              margin-bottom: 2rem;
             }
           }
         `}</style>
