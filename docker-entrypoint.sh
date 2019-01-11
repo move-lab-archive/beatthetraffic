a840d07d421aadd82b79c52f4c7334be1cf24d19
Florian <florian.porada@gmail.com>
2019-01-11 16:06:57 +0100

    Add WIP deploy structure

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -v MONGO_INSTANCE ]]; then
-  echo 'found MONGO_INSTANCE var, using external insance'
+  echo 'found MONGO_INSTANCE var, using external instance'
 else
   echo 'no MONGO_INSTANCE var found, starting local mongo instance'
   service mongodb start
